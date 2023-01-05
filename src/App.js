fdfc947caa26b6928a95c253a9b2717864d124f9
jerrymusaga <musagatechnology@gmail.com>
2023-01-05 08:57:09 +0100

    creating components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { useState } from 'react';
+
+import Feed from './pages/feed/Feed';
+import Navigation from './components/navigation/Navigation';
 
 function App() {
+  const [accounts, setAccounts] = useState([]);
   return (
     <div className="App">
-      Reactos
+      <Navigation accounts={accounts} setAccounts={setAccounts} />
+      <Feed accounts={accounts} setAccounts={setAccounts} /> 
     </div>
   );
 }
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <h1>Nav</h1>
+    )
+}
+
+export default Navigation;
