8c0c20c502ef346482d9edc0f931f4f68ba57ffd
jerrymusaga <musagatechnology@gmail.com>
2023-01-05 08:18:56 +0100

    setting up networks on hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,4 +4,13 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.REACT_APP_GOERLI_URL,
+      accounts: [process.env.REACT_APP_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.REACT_APP_ETHERSCAN_API
+  }
 };
