7c8d224b48910467f9eff75f0d677d28cc3e0256
jerrymusaga <musagatechnology@gmail.com>
2023-01-05 07:50:04 +0100

    wrapping deploy script in a try catch

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,12 +5,19 @@ async function main() {
 
   const Reactos = await hre.ethers.getContractFactory("Reactos");
   const reactos = await Reactos.deploy("Reactos", "RAT");
+  
+  try {
+    await reactos.deployed();
+    console.log(
+      `Reactos deployed to ${reactos.address}`
+    );
+  } catch(err){
+    console.log(`Error: ${err.message}`);
+  }
 
-  await reactos.deployed();
+  
 
-  console.log(
-    `Reactos deployed to ${reactos.address}`
-  );
+  
 }
 
 
